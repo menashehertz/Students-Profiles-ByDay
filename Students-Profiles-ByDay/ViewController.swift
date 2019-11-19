9ef88676c72e2c9efcde4e44b40295d02e09b3d3
Steven Hertz <hertzsteven@icloud.com>
2019-11-19 16:53:26 -0500

    Initial Commit

---
+++ Students-Profiles-ByDay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Students-Profiles-ByDay
+//
+//  Created by Steven Hertz on 11/19/19.
+//  Copyright © 2019 DevelopItSolutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
